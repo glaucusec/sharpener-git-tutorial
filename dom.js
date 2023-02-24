10b88783d85ceda4241bf02093e7e419d09df148
Abhishek Baiju <abhishekbaiju@protonmail.com>
2023-02-24 16:33:39 +0530

    task 6

--- dom.js
+++ dom.js
@@ -6,13 +6,18 @@ header.style.borderBottom = 'solid 3px black'
 let items = document.getElementsByClassName('list-group-item');
 
 console.log(items);
-items[2].style.backgroundColor = 'green';
-items[1].style.fontWeight = 'bold';
 
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
+// task 6
+// deliverable
+items[1].style.backgroundColor = 'green';
+items[2].style.visibility = "hidden";
+
+// deliverable
+let item = document.querySelectorAll('.list-group-item');
+item[1].style.color = "#90EE90";
+
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for (let i=0;i<odd.length;i++) {
+    odd[i].style.backgroundColor = 'green';
 }
-
-// task #5
-withoutClass = document.getElementsByClassName("without-class")[0];
-withoutClass.innerText = "This is the Item #5";
