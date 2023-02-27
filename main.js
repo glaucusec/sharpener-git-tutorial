7c5e28a41f5b474a83968e829a60ff2192aaa87a
Abhishek Baiju <abhishekbaiju@protonmail.com>
2023-02-27 11:11:41 +0530

    task 12

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 let form = document.querySelector('#my-form');
+let itemList = document.querySelector('#items');
 form.addEventListener('submit', saveItems);
 
 function saveItems(e) {
     e.preventDefault();
-    
+
     fName = document.getElementById('name').value;
     fEmail = document.getElementById('email').value;
+    fPhone = document.getElementById('phone').value;
+
 
     userDetails = {
         'userName': fName,
-        'userEmail': fEmail 
+        'userEmail': fEmail,
+        'userPhone': fPhone
     };
 
     // converting object & storing as string
-    localStorage.setItem('userDetails', JSON.stringify(userDetails));
+    localStorage.setItem(fEmail, JSON.stringify(userDetails));
 
-    // accessing and converting back string to object
-    let deSerialized = JSON.parse(localStorage.getItem('userDetails'));
-    console.log(deSerialized); 
+    newLi = document.createElement('li');
+    newLi.appendChild(document.createTextNode(fName
+        + ' - ' + fEmail + ' - ' + fPhone));
+    itemList.appendChild(newLi);
 }
