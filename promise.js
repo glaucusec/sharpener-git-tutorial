0be088e25a3fa716c804dc5fa3177fc60b0385e7
Abhishek Baiju <abhishekbaiju@protonmail.com>
2023-03-10 18:14:34 +0530

    aync await

--- promise.js
+++ promise.js
@@ -1,30 +1,49 @@
-const posts = [{'title': 'POST1'}];
-const user = {
-	name: 'admin', 
-  lastactivitytime: ' '
+const posts = [{ title: "POST1" }];
+const user = { name: "admin", lastActivityTime: "" };
+
+const createPost = (title) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const post = { title: title };
+      resolve(post);
+    }, 1000);
+  });
 };
 
-function createPost(title) {
-    return new Promise( (resolve, reject) => {
-        setTimeout( () => {
-            posts.push({title: title});
-            updateLastUserActivityTime();
-            resolve(posts[posts.length - 1])
-        }, 1000)
-    }) 
-}
+const updateLastUserActivityTime = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const now = new Date();
+      resolve(now);
+    }, 1000);
+  });
+};
 
-function updateLastUserActivityTime (){
-    return new Promise((resolve, reject) => {
-        setTimeout( () => {
-          user.lastactivitytime = new Date();
-          resolve(user.lastactivitytime);
-        }, 1000)
-    })
-}
+const deletePost = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const deletedPost = posts.pop();
+      resolve(deletedPost);
+    }, 1000);
+  });
+};
 
-Promise.all([createPost('POST2'), updateLastUserActivityTime()])
-	.then(([val1, val2]) => {
-  	console.log(val1)
-    console.log(val2)
+const preTasks = async (title) => {
+  const [createdPost, lastActivityTime] = await Promise.all([
+    createPost(title),
+    updateLastUserActivityTime(),
+  ]);
+
+  const deletedPost = await deletePost();
+
+  return [createdPost, lastActivityTime, deletedPost];
+};
+
+
+preTasks("POST2")
+  .then(([createdPost, lastActivityTime, deletedPost]) => {
+    console.log(createdPost);
+    console.log(lastActivityTime);
+    console.log(deletedPost);
   })
+  .catch((error) => console.error(error));
