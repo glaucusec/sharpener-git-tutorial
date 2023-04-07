bd1d89b028eafa46cc3df3670324c972395f9805
Abhishek Baiju <abhishekbaiju@protonmail.com>
2023-04-07 11:02:31 +0530

    request / response

--- creating-node-server/app.js
+++ creating-node-server/app.js
@@ -1,9 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if(req) {
-        console.log("Abhishek")
+
+    if(req.url == '/home') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h1>Welcome home</h1>');
+        res.end();
+    } else if(req.url == '/about') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h1>Welcome to About Us Page');
+        res.end();
+    } else if(req.url == '/node') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h1>Welcome to my Node Js project</h1>')
+        res.end();
     }
 });
 
-server.listen(3000);
+server.listen(3000); 
