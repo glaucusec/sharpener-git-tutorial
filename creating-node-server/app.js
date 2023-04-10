3c9e920eb8e1d330d38b039d432bf7f6a0fc5a42
Abhishek Baiju <abhishekbaiju@protonmail.com>
2023-04-10 11:31:17 +0530

    Intro to Middlewares

--- creating-node-server/app.js
+++ creating-node-server/app.js
@@ -1,17 +1,22 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // allows the request to continue to next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text", name="title"><br><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('{ key1: value }');
+app.post('/product', (req, res, next) => {
+    console.log('Title: ' + req.body['title']);
+    console.log('Size: ' + req.body['size']);
+    res.redirect('/');
+})
+   
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express</h1>');
 });
 
 app.listen(3000);
