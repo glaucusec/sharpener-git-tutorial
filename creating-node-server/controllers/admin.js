2f6143df3399175caa063d8b994e063cd8be75a2
Abhishek Baiju <abhishekbaiju@protonmail.com>
2023-04-18 19:59:20 +0530

    Making SQL easy with Sequelize

--- creating-node-server/controllers/admin.js
+++ creating-node-server/controllers/admin.js
@@ -13,13 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch((err) => console.log(err));
+  Product.create({
+    title: title, 
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+  .then(result => {
+    // console.log(result);
+    console.log('Created Product');
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -28,17 +35,21 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
+  Product.findAll({
+    where: {
+      id: prodId
     }
+  })
+  .then(product => {
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product
+      product: product[0]
     });
-  });
+  })
+  .catch(err => console.log(err));
+  
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -47,22 +58,27 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.update( {
+    id: prodId,
+    title: updatedTitle,
+    price: updatedPrice,
+    imageUrl: updatedImageUrl,
+    description: updatedDesc
+  }, {
+    where: {
+      id: prodId
+    }
+  })
+  .then(res.redirect('/admin/products'))
+  .catch(err => console.log(err));
+  
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('admin/products', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products'
       });
@@ -73,7 +89,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-    .then(res.redirect('/admin/products'))
-    .catch((err) => console.log(err));
+  Product.destroy( {
+    where: {
+      id: prodId
+    }
+  })
+  .then(res.redirect('/admin/products'))
+  .catch(err => console.log(err));
 }
